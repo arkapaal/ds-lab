9c8fde859c48c05f252e7eed69cab16500ef4c66
Arka Pal <156821082+arkapaal@users.noreply.github.com>
2024-08-07 20:21:54 +0530

    Add files via upload

---
+++ comparetwonumbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void compare(int *num1, int *num2) {
+    if (*num1 > *num2) {
+        printf("%d is greater than %d\n", *num1, *num2);
+    } else if (*num1 < *num2) {
+        printf("%d is less than %d\n", *num1, *num2);
+    } else {
+        printf("%d is equal to %d\n", *num1, *num2);
+    }
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    compare(&num1, &num2);
+
+    return 0;
+}
